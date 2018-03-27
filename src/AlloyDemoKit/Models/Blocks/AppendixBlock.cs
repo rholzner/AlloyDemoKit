ff03ffec72ac213d4e52afe7c522f8f7f7e67fb2
Robin Holzner <robin.holzner@epinova.no>
2018-03-27 14:45:30 +0200

    AppendixBlock: added block that is using Ilist with url property

---
+++ src/AlloyDemoKit/Models/Blocks/AppendixBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using AlloyDemoKit.PropertyDefinition;
+using EPiServer;
+using EPiServer.Cms.Shell.UI.ObjectEditing.EditorDescriptors;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Shell.ObjectEditing;
+
+namespace AlloyDemoKit.Models.Blocks
+{
+    [ContentType(GUID = "cb788228-13ce-4cae-aee3-9e08261ccc8f")]
+    [ImageUrl("~/UI/epi-thumbs/add.svg")]
+    public class AppendixBlock : SiteBlockData
+    {
+        [CultureSpecific]
+        [Display(Order = 100, GroupName = SystemTabNames.Content)]
+        public virtual string Heading { get; set; }
+
+        public virtual Url Url { get; set; }
+
+
+        [Required]
+        [CultureSpecific]
+        [EditorDescriptor(EditorDescriptorType = typeof(CollectionEditorDescriptor<AppendixValueItem>))]
+        [Display(Order = 200, GroupName = SystemTabNames.Content)]
+        public virtual IList<AppendixValueItem> AppendixItems { get; set; }
+
+        public bool HasHeading => !string.IsNullOrEmpty(Heading);
+    }
+}
---
+++ src/AlloyDemoKit/PropertyDefinition/PropertyListBase.cs
@@ -0,0 +1,39 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.Serialization;
+using EPiServer.Framework.Serialization.Internal;
+using EPiServer.PlugIn;
+using EPiServer.ServiceLocation;
+
+namespace AlloyDemoKit.PropertyDefinition
+{
+    public class PropertyListBase<T> : PropertyList<T>
+    {
+        public PropertyListBase()
+        {
+            _objectSerializer = this._objectSerializerFactory.Service.GetSerializer("application/json");
+        }
+
+        private Injected<ObjectSerializerFactory> _objectSerializerFactory;
+        private IObjectSerializer _objectSerializer;
+
+        protected override T ParseItem(string value)
+        {
+            return _objectSerializer.Deserialize<T>(value);
+        }
+
+    }
+
+    [PropertyDefinitionTypePlugIn]
+    public class AppendixTypeProperty : PropertyListBase<AppendixValueItem>
+    {
+    }
+
+    public class AppendixValueItem
+    {
+        public int AppendixType { get; set; }
+
+        public string Text { get; set; }
+        public Url Url { get; set; }
+    }
+}
